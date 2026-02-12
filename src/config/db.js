9753316aa5555d0d33734b1b1bee9cc79b37a029
Tanishk Sancheti <tanishksancheti1@gmail.com>
2026-02-12 10:46:56 +0530

    fixed

--- src/config/db.js
+++ src/config/db.js
@@ -8,15 +8,9 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    logging: false
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false
+});
 
 export default sequelize;
